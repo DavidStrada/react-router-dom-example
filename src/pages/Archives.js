d46dfe7237d5eb915cb269222d6bf932cdfcbeeb
David S <idavid.str@gmail.com>
2017-02-25 11:39:12 -0600

    Components, Layouts, Pages.

---
+++ src/pages/Archives.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import { Router, Route, Link } from 'react-router'
+
+class Archives extends Component {
+
+  render () {
+    return (
+      <h1>Archives Page</h1>
+    )
+  }
+}
+
+export default Archives;
---
+++ src/pages/Features.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import { Router, Route, Link } from 'react-router'
+
+class Features extends Component {
+
+  render () {
+    return (
+      <h1>Features Page</h1>
+    )
+  }
+}
+
+export default Features;
