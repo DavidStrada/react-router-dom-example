9bd6f4c5ee88ba1560992517dbe78b4fabbbfef7
David S <idavid.str@gmail.com>
2017-02-26 11:41:25 -0600

    App handleling Layout.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom'
-// import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import {
-  BrowserRouter as Router,
-  Route,
-  // etc.
-} from 'react-router-dom'
-// import createBrowserHistory from 'history/createBrowserHistory'
-import App from './App';
-import Layout from './components/Layout'
-import Features from './pages/Features'
-import Archives from './pages/Archives'
 
-// const history = createBrowserHistory()
+import App from './App';
 
 render(
   <App />,
