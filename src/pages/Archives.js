af28d28aa5fc3db569e8aca1aedc43112672d62d
David S <idavid.str@gmail.com>
2017-02-26 11:41:42 -0600

    Moved to partials

--- src/pages/Archives.js
+++ src/pages/Archives.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 // import { Router, Route, Link } from 'react-router'
 
 class Archives extends Component {
+  constructor (match) {
+    super()
+    // {match.params.userId} // e.g.userId
+    // console.log(match)
+  }
 
   render () {
     return (
