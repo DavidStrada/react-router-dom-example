72769df30fc0accaf2fc6940fa573d065723e62b
David S <idavid.str@gmail.com>
2017-02-26 11:41:00 -0600

    Component base app

--- src/App.js
+++ src/App.js
@@ -1,60 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+   BrowserRouter as Router
 } from 'react-router-dom'
 
-const BasicExample = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
+// Pages Routes
+import {routes} from './routes/routes'
 
-      <hr/>
+// Styles
+import './App.css'
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
-    </div>
-  </Router>
-)
+// Pages Navigation
+import Nav from './partials/Nav'
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
+// Pages Main Content
+import MainContent from './partials/MainContent'
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+// Pages Footer
+import Footer from './partials/Footer'
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
-      <li><Link to={`${match.url}/components`}>Components</Link></li>
-      <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
-    </ul>
 
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav /> 
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
+          <MainContent routes={routes} />
 
-export default BasicExample
+          <Footer />
+        </div>
+      </Router>
+
+
+    )
+  }
+}
+
+export default App;
